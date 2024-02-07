454ad79f3708aef13cfccde344afd06b0ff2f37c
chanpreetsingh <chanpreetsingh73553@gmail.com>
2024-02-07 12:07:17 +0530

    create categorycard component

---
+++ src/component/CategoryCard.tsx
@@ -0,0 +1,19 @@
+import { RiArrowRightLine } from "@remixicon/react";
+import { Categories } from "../store/api";
+
+export default function CategoryCard(category: Categories) {
+  return (
+    <div key={category.id}>
+      <img src={category.img} alt="categories" className="rounded-md" />
+      <div className="flex items-center justify-between mt-3">
+        <div>
+          <h5 className="font-semibold pb-0.5">{category.name}</h5>
+          <p className="text-xs text-gray-500">Explore Now!</p>
+        </div>
+        <div>
+          <RiArrowRightLine className="size-5 text-gray-500" />
+        </div>
+      </div>
+    </div>
+  );
+}
