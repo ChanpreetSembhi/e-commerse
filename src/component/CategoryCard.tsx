5ad65f1c9d0c6f30493229b1cf0f453156d94d91
chanpreetsingh <chanpreetsingh73553@gmail.com>
2024-02-09 11:51:34 +0530

    style improvement in category card component

--- src/component/CategoryCard.tsx
+++ src/component/CategoryCard.tsx
@@ -4,10 +4,10 @@ import { Categories } from "../store/api";
 export default function CategoryCard(category: Categories) {
   return (
     <div key={category.id}>
-      <img src={category.img} alt="categories" className="rounded-md" />
+      <img src={category.img} alt="categories" className="rounded-xl" />
       <div className="flex items-center justify-between mt-3">
         <div>
-          <h5 className="font-semibold pb-0.5">{category.name}</h5>
+          <h5 className="font-semibold pb-0.5 text-sm">{category.name}</h5>
           <p className="text-xs text-gray-500">Explore Now!</p>
         </div>
         <div>
